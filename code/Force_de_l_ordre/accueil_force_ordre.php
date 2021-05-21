32490d01ec61cb278e8362bddbe3c7651f6c3094
Huclas <lucashubert60@gmail.com>
2021-05-21 12:32:38 +0200

    ajout de l'affichage des documents pour les force de l'ordre

--- code/Force_de_l_ordre/accueil_force_ordre.php
+++ code/Force_de_l_ordre/accueil_force_ordre.php
@@ -8,6 +8,17 @@
 		<meta charset="utf-8">
 	</head>
 	<body>
+		<?php
+			$nom = $_GET["nom"];
+			$prenom = $_GET["prenom"];
+			$assurance	= $_GET["assurance"];
+			$path = "../../database/".$assurance."/".$nom[0]."/".$nom."_".$prenom."/Documents/";
+			echo '<object data='.$path.'fichier.pdf type="application/pdf" width="700" height="400">
+  				<param name="filename" value='.$path.'fichier.pdf /> 
+  				<a href='.$path.'fichier.pdf title="le fichier">Téléchargez le fichier...</a>
+			</object>';
+
+		?>
         <div>Affichage informations utilisateur scanné</div>  
 	</body>
 </html>
